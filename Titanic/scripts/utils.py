07afe3a218e470fbb5093cd1f140686cd8d14a03
dvomlel <danilovomlel@gmail.com>
2022-08-14 19:10:28 -0300

    funcoes auxiliares para os .ipynb

---
+++ Titanic/scripts/utils.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+def get_project_root() -> Path:
+    #root = Path(__file__).parent.parent
+    root = "C:/Users/Pristino/Downloads/Projetos-prog/Kaggle/Titanic"
+    return root
