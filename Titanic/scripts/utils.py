5a0f6d95c2a50c6b00fa232df4d6b4be6ebd6bf3
dvomlel <danilovomlel@gmail.com>
2022-08-15 13:04:10 -0300

    utils.py refatorado e adicao de .pickle em gitignore

--- Titanic/scripts/utils.py
+++ Titanic/scripts/utils.py
@@ -1,18 +1,15 @@
+"Funções utilizadas nos .ipynb"
+
 from pathlib import Path
 import pandas as pd
 
-def get_project_root() -> Path:
-    #root = Path(__file__).parent.parent
-    root = "C:/Users/Pristino/Downloads/Projetos-prog/Kaggle/Titanic"
-    return root
-
-#Insere a idade baseado na média de idade por classe social
 IDADE_CLASSE = {1: 38.2, 2: 29.9, 3: 25.1}
 def impute_age(cols):
+    "Insere a idade baseado na média de idade por classe social"
     Age = cols[0]
     Pclass = cols[1]
-    
-    if pd.isnull(Age):
+
+    if pd.isna(Age):
         if Pclass == 1:
             return IDADE_CLASSE[1]
         elif Pclass == 2:
